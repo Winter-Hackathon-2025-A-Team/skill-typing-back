7f0aa134f080fc56d48bf8d06c8314df9a14ba77
finish-cloud <hope.is.a.waking.dream.8.1@gmail.com>
2025-01-23 21:44:18 +0900

    環境変数の設定する準備

--- db/db.go
+++ db/db.go
@@ -9,3 +9,12 @@ import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 }
+
+func NewDB() *gorm.DB {
+	if os.Getenv("GO_ENV") == "dev" {
+		err := gotoenv.Load{}
+		if err!= nil {
+            log.Fatal(err)
+        }
+	}
+}
