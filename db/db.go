6d6004c6dd57763e2e8e7e1fad79e8d935c44a8f
finish-cloud <hope.is.a.waking.dream.8.1@gmail.com>
2025-01-25 21:00:46 +0900

    importの)を修正

--- db/db.go
+++ db/db.go
@@ -1,32 +1,32 @@
 package db
 
 import (
-    "fmt"
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-}
+)
 
 func NewDB() *gorm.DB {
 	if os.Getenv("GO_ENV") == "dev" {
 		err := gotoenv.Load{}
-		if err!= nil {
-            log.Fatal(err)
-        }
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 	url := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True",
 		os.Getenv("MYSQL_USER"),
 		os.Getenv("MYSQL_PW"),
 		os.Getenv("MYSQL_PORT"),
 		os.Getenv("MYSQL_HOST"),
-        os.Getenv("MYSQL_DB"))
+		os.Getenv("MYSQL_DB"))
 	db, err := gorm.Open(mysql.Open(url), &gorm.Config{})
 	if err != nil {
-        log.Fatal(err)
-    }
+		log.Fatal(err)
+	}
 	fmt.Println("Connected")
 	return db
 }
