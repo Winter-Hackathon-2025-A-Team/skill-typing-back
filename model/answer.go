5b9a62b67dcd09930819d8b642daf7822aec7327
finish-cloud <hope.is.a.waking.dream.8.1@gmail.com>
2025-02-03 19:46:23 +0900

    user.goを修正

--- model/answer.go
+++ model/answer.go
@@ -1,11 +1,11 @@
 package model
 
 type Answer struct {
-	ID         int `json:"id" gorm:"PrimaryKey;Autoincrement"`
-	QuestionID int `json:"question_id" gorm:"not null"`
-	ChoiceID   int `json:"choice_id" gorm:"not null"`
+	ID         uint `json:"id" gorm:"PrimaryKey;Autoincrement"`
+	QuestionID uint `json:"question_id" gorm:"not null"`
+	ChoiceID   uint `json:"choice_id" gorm:"not null"`
 
 	// リレーション（ポインタ型で再帰を避ける）
-	Question *Question `gorm:"foreignKey:QuestionID"` // 質問とのリレーション：ポインタ型に変更
-	Choice   *Choice   `gorm:"foreignKey:ChoiceID"`   // 選択肢とのリレーション：ポインタ型に変更
+	Question *Question `gorm:"foreignKey:QuestionID;references:ID;constraint:OnDelete:CASCADE;"` // 質問とのリレーション
+	Choice   *Choice   `gorm:"foreignKey:ChoiceID;references:ID;constraint:OnDelete:CASCADE;"`   // 選択肢とのリレーション
 }
