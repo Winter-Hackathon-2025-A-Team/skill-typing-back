fd09b276df7490440c75893f1b0529ece96223f9
finish-cloud <hope.is.a.waking.dream.8.1@gmail.com>
2025-02-03 19:48:37 +0900

    score.goにカスケードを追加

--- model/score.go
+++ model/score.go
@@ -3,11 +3,11 @@ package model
 import "time"
 
 type Score struct {
-	ID        int       `json:"id" gorm:"primaryKey;autoIncrement"` // 主キー
-	UserID    int       `json:"user_id" gorm:"not null"`            // 関連するユーザー
-	Score     int       `json:"score" gorm:"not null"`              // スコア
+	ID        uint      `json:"id" gorm:"primaryKey;autoIncrement"` // 主キー
+	UserID    uint      `json:"user_id" gorm:"not null;index"`      // 関連するユーザー
+	Score     uint      `json:"score" gorm:"not null"`              // スコア
 	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`   // 問題実行日時
 
 	// リレーション
-	User User `gorm:"foreignKey:UserID"` // ユーザーとのリレーション
+	User User `gorm:"foreignKey:UserID;references:ID;constraint:OnDelete:CASCADE;"` //  外部キー制約の明示
 }
