26452b0bc591169a2e43a9b36bc40b6b0f023758
finish-cloud <hope.is.a.waking.dream.8.1@gmail.com>
2025-02-03 19:47:32 +0900

    choice.goをmigrateできるように修正

--- model/choice.go
+++ model/choice.go
@@ -3,14 +3,14 @@ package model
 import "time"
 
 type Choice struct {
-	ID          int       `json:"id" gorm:"primaryKey;Autoincrement"`    // 主キー
-	QuestionID  int       `json:"question_id" gorm:"not null"`           // 関連する質問
+	ID          uint      `json:"id" gorm:"primaryKey;autoIncrement"`    // 主キー
+	QuestionID  uint      `json:"question_id" gorm:"not null;index"`     // 関連する質問
 	Content     string    `json:"content" gorm:"type:text;not null"`     // 問題の内容
 	Description string    `json:"description" gorm:"type:text;not null"` // 問題の解説
 	CreatedAt   time.Time `json:"created_at" gorm:"autoCreateTime"`      // 作成日時
 	UpdatedAt   time.Time `json:"updated_at" gorm:"autoUpdateTime"`      // 更新日時
 
 	// リレーション
-	Question Question `gorm:"foreignKey:QuestionID"`
-	Answers  []Answer `gorm:"foreignKey:ChoiceID"` // 答えとのリレーション
+	Question Question `gorm:"foreignKey:QuestionID;references:ID;constraint:OnDelete:CASCADE;"` // ✅ 外部キー制約の修正
+	Answers  []Answer `gorm:"foreignKey:ChoiceID;references:ID;constraint:OnDelete:CASCADE;"`   // ✅ Answerとのリレーション明確化
 }
