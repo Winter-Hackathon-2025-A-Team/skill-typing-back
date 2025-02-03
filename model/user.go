af719f9f53f43bb7505bb20429d6cd2ac263f138
finish-cloud <hope.is.a.waking.dream.8.1@gmail.com>
2025-02-03 19:49:04 +0900

    user.goをmigrateできるように修正

--- model/user.go
+++ model/user.go
@@ -3,14 +3,14 @@ package model
 import "time"
 
 type User struct {
-	ID        int       `json:"id" gorm:"primaryKey;autoIncrement"`    // ID
-	CognitoID string    `json:"cognitoID" gorm:"size:255;uniqueIndex"` // AWS CognitoID
-	IsAdmin   bool      `json:"is_admin" gorm:"default:false"`         // 管理者かどうか
-	Name      string    `json:"name" gorm:"size:100;not null"`         // ユーザー名
-	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`      // 作成日時
-	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`      // 更新日時
+	ID        uint      `json:"id" gorm:"primaryKey;autoIncrement"` // ID
+	CognitoID string    `json:"cognitoID" gorm:"size:255;unique"`   // AWS CognitoID（修正）
+	IsAdmin   bool      `json:"is_admin" gorm:"default:false"`      // 管理者かどうか
+	Name      string    `json:"name" gorm:"size:100;not null"`      // ユーザー名
+	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`   // 作成日時
+	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`   // 更新日時
 
 	// リレーション
-	Questions []Question `gorm:"foreignKey:UserID"` // ユーザーが作成した質問
-	Scores    []Score    `gorm:"foreignKey:UserID"` // ユーザーのスコア
+	Questions []Question `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE;"` // ユーザーが作成した質問
+	Scores    []Score    `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE;"` // ユーザーのスコア
 }
