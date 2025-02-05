8dccc27ed9cd1cc968baadf80de4c75c3f878572
finish-cloud <hope.is.a.waking.dream.8.1@gmail.com>
2025-02-06 00:28:39 +0900

    [feat]router.goにapiを追加

--- router/router.go
+++ router/router.go
@@ -1,1 +1,24 @@
 package router
+
+import (
+	"skill-typing-back/handler"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func SetupRouter() *echo.Echo {
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// ルーティング
+	api := e.Group("/api")
+
+	// 質問の作成エンドポイント
+	api.POST("/questions", handler.CreateQuestion)
+
+	return e
+}
