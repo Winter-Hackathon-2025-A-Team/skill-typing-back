1550132e8bb26cb31363e3b839f4c09330f5b3f2
finish-cloud <hope.is.a.waking.dream.8.1@gmail.com>
2025-02-14 22:01:51 +0900

    [fix]answer.goを修正

--- model/answer.go
+++ model/answer.go
@@ -1,9 +1,13 @@
 package model
 
+import "time"
+
 type Answer struct {
-	ID         uint `json:"id" gorm:"PrimaryKey;Autoincrement"`
-	QuestionID uint `json:"question_id" gorm:"not null"`
-	ChoiceID   uint `json:"choice_id" gorm:"not null"`
+	ID          uint      `json:"id" gorm:"PrimaryKey;Autoincrement"`
+	QuestionID  uint      `json:"question_id" gorm:"not null"`
+	ChoiceID    uint      `json:"choice_id" gorm:"not null"`
+	Explanation string    `json:"explanation"`
+	CreatedAt   time.Time `json:"created_at" gorm:"autoCreateTime"` // 問題実行日時
 
 	// リレーション（ポインタ型で再帰を避ける）
 	Question *Question `gorm:"foreignKey:QuestionID;references:ID;constraint:OnDelete:CASCADE;"` // 質問とのリレーション
