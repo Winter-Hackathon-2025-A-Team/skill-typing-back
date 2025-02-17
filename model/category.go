0c0fb6d1939269aacd9538d2ab92f61526256aed
finish-cloud <hope.is.a.waking.dream.8.1@gmail.com>
2025-02-17 23:58:28 +0900

    [fix]model/category.go作成日時を追加

--- model/category.go
+++ model/category.go
@@ -1,8 +1,11 @@
 package model
 
+import "time"
+
 type Category struct {
-	ID    uint   `json:"id" gorm:"primaryKey;autoIncrement"` // 主キー
-	Title string `json:"title" gorm:"size:255;not null"`     // カテゴリー名（ユニーク）
+	ID        uint      `json:"id" gorm:"primaryKey;autoIncrement"` // 主キー
+	Title     string    `json:"title" gorm:"size:255;not null"`     // カテゴリー名（ユニーク）
+	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`   // 作成日時
 
 	// リレーション
 	Questions []Question `gorm:"foreignKey:CategoryID;constraint:OnDelete:CASCADE;"` //  外部キーの設定
