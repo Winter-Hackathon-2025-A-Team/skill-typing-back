6af12e7e50e76a40cbd149cef45472ab4ea55631
finish-cloud <hope.is.a.waking.dream.8.1@gmail.com>
2025-02-25 22:44:57 +0900

    [fix]ゲーム画面の追加の取得エンドポイントを追加

--- router/router.go
+++ router/router.go
@@ -82,7 +82,9 @@ func SetupRouter() *echo.Echo {
 	//  AI 生成クイズ API
 	api.GET("/generate-quiz", apiHandler.GenerateQuizHandler)
 	//ゲーム画面の取得エンドポイント
-	e.GET("api/game/questions", apiHandler.GetGameQuestions)
+	api.GET("/game/questions", apiHandler.GetGameQuestions)
+	//ゲーム画面の追加の取得エンドポイント
+	api.GET("/game/questions/additional", apiHandler.GetAdditionalGameQuestions)
 
 	return e
 }
